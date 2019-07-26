428bdc856e963df2a55293ededa7d83b8fa91818
mafei <535977672@qq.com>
2019-07-26 17:50:08 +0800

    '1'

---
+++ gitpush.bat
@@ -0,0 +1,2 @@
+@echo off && git status >nul 2>nul && git add . >nul 2>nul && git add . >nul 2>nul && git add . >nul 2>nul && git commit -m '1' >nul 2>nul && git pull >nul 2>nul && git push >nul 2>nul && @echo on
+pause
